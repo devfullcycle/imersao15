9b1d2abd0b67c0bb00b02624417e7096d67fb666
JorgeFPeres <jorge-peres@live.com>
2023-10-16 17:09:38 -0300

    test: typo

--- domain/model/account_test.go
+++ domain/model/account_test.go
@@ -21,7 +21,7 @@ func TestModel_NewAccount(t *testing.T) {
 	require.Nil(t, err)
 	require.NotEmpty(t, uuid.FromStringOrNil(account.ID))
 	require.Equal(t, account.Number, accountNumber)
-	require.Equal(t, account.BankID, bank.ID)
+	require.Equal(t, account.Bank.ID, bank.ID)
 
 	_, err = model.NewAccount(bank, "", ownerName)
 	require.NotNil(t, err)
